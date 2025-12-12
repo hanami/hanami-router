848301a9842bcbcbaa364abeeb3053980703331d
Andreas Haller <andreas.haller@peopleware.com>
2025-12-12 05:21:22 +0100

    Reactivate tests about SCRIPT_NAME (#292)
    One of the these tests is passing, the other two are marked as pending, because the described behavior is probably how the router should behave.

--- spec/integration/hanami/router/request_url_spec.rb
+++ spec/integration/hanami/router/request_url_spec.rb
@@ -2,12 +2,12 @@
 
 require "rack/test"
 
-RSpec.xdescribe "SCRIPT_NAME" do
+RSpec.describe "SCRIPT_NAME" do
   include Rack::Test::Methods
 
   before do
     @container = Hanami::Router.new do
-      @some_test_router = Hanami::Router.new(prefix: "/admin") do
+      @some_test_router = Hanami::Router.new do
         get "/foo", to: ->(env) { [200, {}, [::Rack::Request.new(env).url]] }, as: :foo
       end
       mount @some_test_router, at: "/admin"
@@ -26,26 +26,26 @@ RSpec.xdescribe "SCRIPT_NAME" do
     last_request
   end
 
-  it "generates proper path" do
+  pending "generates proper path" do
     router = @container.instance_variable_get(:@some_test_router)
     expect(router.path(:foo)).to eq("/admin/foo")
   end
 
-  it "generates proper url" do
+  pending "generates proper url" do
     router = @container.instance_variable_get(:@some_test_router)
-    expect(router.url(:foo)).to eq("http://localhost/admin/foo")
+    expect(router.url(:foo).to_s).to eq("http://localhost/admin/foo")
   end
 
-  it "is successfuly parsing a JSON body" do
-    script_name = "/admin/foo"
-    get script_name
+  it "sets SCRIPT_NAME to the mount point" do
+    path = "/admin/foo"
+    get path
 
     expect(response.status).to eq(200)
-    expect(request.env["SCRIPT_NAME"]).to eq(script_name)
+    expect(request.env["SCRIPT_NAME"]).to eq("/admin")
     expect(request.env["SCRIPT_NAME"]).to be_kind_of(String)
 
-    expect(request.env["PATH_INFO"]).to eq("")
+    expect(request.env["PATH_INFO"]).to eq("/foo")
     expect(request.env["PATH_INFO"]).to be_kind_of(String)
-    expect(response.body).to eq("http://example.org#{script_name}")
+    expect(response.body).to eq("http://example.org#{path}")
   end
 end
