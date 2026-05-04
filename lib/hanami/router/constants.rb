866895be134f3b3410adea37aa3482b93caa5840
Sean Collins <sean@cllns.com>
2026-05-03 20:51:18 -0600

    Extract constant for media type

--- lib/hanami/router/constants.rb
+++ lib/hanami/router/constants.rb
@@ -5,5 +5,9 @@ module Hanami
     # @api private
     # @since 2.0.0
     ROUTER_PARSED_BODY = "router.parsed_body"
+
+    # @api private
+    # @since x.x.x
+    FORM_URLENCODED_MEDIA_TYPE = "application/x-www-form-urlencoded"
   end
 end
