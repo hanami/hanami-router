14f8d489e8622901be9f5310f4e45017c52877cf
Edouard <edouard@melix.net>
2026-05-15 05:34:43 +0200

    Add array variables extraction + injection around Mustermann expand method (#304)
    Add array variables extraction + injection around Mustermann expand method
    
    ---------
    
    Co-authored-by: Tim Riley <tim@riley.id.au>

--- lib/hanami/router/url_helpers.rb
+++ lib/hanami/router/url_helpers.rb
@@ -6,7 +6,6 @@ require_relative "prefix"
 
 module Hanami
   class Router
-    # @since 2.0.0
     # @api private
     class UrlHelpers
       # @since 2.0.0
@@ -19,26 +18,39 @@ module Hanami
         @prefix = Prefix.new(prefix)
       end
 
-      # @since 2.0.0
       # @api private
       def add(name, segment)
         @named[name] = segment
       end
 
-      # @since 2.0.0
       # @api private
       def path(name, variables = {})
-        @named.fetch(name.to_sym) do
-          raise MissingRouteError.new(name)
-        end.expand(:append, variables)
+        scalar_vars = variables.reject { |_, value| value.is_a?(Array) }
+        array_vars = array_query_vars(variables)
+
+        expanded_path = @named
+          .fetch(name.to_sym) { raise MissingRouteError.new(name) }
+          .expand(:append, scalar_vars)
+
+        return expanded_path if array_vars.empty?
+
+        join_char = expanded_path.include?("?") ? "&" : "?"
+        "#{expanded_path}#{join_char}#{Rack::Utils.build_query(array_vars)}"
       rescue Mustermann::ExpandError => exception
         raise InvalidRouteExpansionError.new(name, exception.message)
       end
 
-      # @since 2.0.0
       # @api private
       def url(name, variables = {})
         @base_url + @prefix.join(path(name, variables)).to_s
+      end
+
+      private
+
+      def array_query_vars(variables = {})
+        variables
+          .select { |_, value| value.is_a?(Array) }
+          .to_h { |key, value| ["#{key}[]", value] }
       end
     end
   end
